c204a680e0c31b3e73f3eb8f82290fbf03cc061c
Eduardo Silva <edu@localhost.localdomain>
2025-08-11 18:41:27 -0300

    added Project

---
+++ composeApp/src/androidUnitTest/kotlin/org/mdmsystemtools/application/ComposeAppAndroidUnitTest.kt
@@ -0,0 +1,12 @@
+package org.mdmsystemtools.application
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppAndroidUnitTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
