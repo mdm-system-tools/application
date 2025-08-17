9e5d7c8a2d047f9e154f9919842d3e717b08111c
Matheus <Matheusssousaribeiro@gmail.com>
2025-08-17 14:55:14 -0300

    Tela Cadastros.kt mais organizada, e refatorada

---
+++ composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Cadastro/homeScreen.kt
@@ -0,0 +1,20 @@
+package org.mdmsystemtools.application.Telas.Cadastro
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import org.mdmsystemtools.application.componentes.ButtonFormAdd
+
+@Composable
+fun CadastrosScreen() {
+  var showDialog by remember { mutableStateOf(false) }
+
+  ButtonFormAdd(onClick = { showDialog = true })
+
+  Formulario(
+    showDialog = showDialog,
+    onDismiss = { showDialog = false }
+  )
+}
