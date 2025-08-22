c04b45a7dc1054f563966e657ee3ae5bb2c3da20
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-22 16:52:36 -0300

    fix: button function

--- composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Cadastro/homeScreen.kt
+++ composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Cadastro/homeScreen.kt
@@ -5,13 +5,16 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import org.mdmsystemtools.application.componentes.ButtonFormAdd
 
 @Composable
 fun CadastrosScreen() {
   var showDialog by remember { mutableStateOf(false) }
+	val modifier: Modifier = Modifier
 
-  ButtonFormAdd(onClick = { showDialog = true })
+  ButtonFormAdd(modifier, onClick = { showDialog = true })
 
   Formulario(
     showDialog = showDialog,
