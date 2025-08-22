e2cfa5e9c70e08ba59a85a3bed8a86f65e42e4e1
Eduardo Silva <eduardofox989@protonmail.com>
2025-08-22 16:57:19 -0300

    added: separates the apiService from the connectapi file and remove loadAssociated, LaunchedEffect

---
+++ composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Api/ApiService.kt
@@ -0,0 +1,72 @@
+package org.mdmsystemtools.application.Telas.Api
+
+import android.util.Log
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object ApiService {
+	private val client = HttpClient(Android) {
+		install(ContentNegotiation) {
+			json(Json {
+				prettyPrint = true
+				isLenient = true
+				ignoreUnknownKeys = true
+			})
+		}
+	}
+	private const val BASE_URL = "http://10.0.2.2:8080"
+	suspend fun getAllAssociated(): List<AssociatedDto> {
+		try {
+			val httpResponse: HttpResponse = client.get("$BASE_URL/associated")
+			if (httpResponse.status.value == HttpStatusCode.Conflict.value) {
+				Log.e("ConnectAPI", "Failed to get response: ${httpResponse.body() as String}")
+				return emptyList()
+			}
+			if (httpResponse.status.value == HttpStatusCode.OK.value) {
+				Log.i("ConnectAPI", "Success to Get response")
+			}
+			return httpResponse.body() as List<AssociatedDto>
+		} catch (e: Exception) {
+			Log.e("ConnectAPI", "Failed to get response", e)
+			e.printStackTrace()
+			return emptyList()
+		}
+	}
+
+	suspend fun postAssociated(associated: AssociatedDto): Boolean {
+		try {
+			val httpResponse: HttpResponse = client.post("$BASE_URL/associated") {
+				contentType(ContentType.Application.Json)
+				setBody(associated)
+			}
+			if (httpResponse.status.value == HttpStatusCode.Conflict.value) {
+				Log.e("ConnectAPI", "Failed to get response: ${httpResponse.body() as String}")
+				return false
+			}
+			if (httpResponse.status.value == HttpStatusCode.BadRequest.value) {
+				Log.e("ConnectAPI", "Failed to get response: ${httpResponse.body() as String}")
+				return false
+			}
+			if (httpResponse.status.value == HttpStatusCode.OK.value) {
+				Log.i("ConnectAPI", "Success to Get response")
+				return true
+			}
+			return true
+		} catch (e: Exception) {
+			Log.e("ConnectAPI", "Failed to get response", e)
+			e.printStackTrace()
+			return false
+		}
+	}
+}
--- composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Api/ConnectAPI.kt
+++ composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Api/ConnectAPI.kt
@@ -1,6 +1,5 @@
 package org.mdmsystemtools.application.Telas.Api
 
-import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,105 +7,26 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.android.Android
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.client.statement.HttpResponse
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.http.contentType
-import io.ktor.serialization.kotlinx.json.json
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlinx.serialization.json.Json
 import org.mdmsystemtools.application.componentes.ButtonFormAdd
-import kotlin.collections.emptyList
-
-
-object ApiService {
-	private val client = HttpClient(Android) {
-		install(ContentNegotiation) {
-			json(Json {
-				prettyPrint = true
-				isLenient = true
-				ignoreUnknownKeys = true
-			})
-		}
-	}
-	private const val BASE_URL = "http://10.0.2.2:8080"
-	suspend fun getAllAssociated(): List<Associated> {
-		try {
-			val httpResponse: HttpResponse = client.get("$BASE_URL/associated")
-			if (httpResponse.status.value == HttpStatusCode.Conflict.value) {
-				Log.e("ConnectAPI", "Failed to get response: ${httpResponse.body() as String}")
-				return emptyList()
-			}
-			if (httpResponse.status.value == HttpStatusCode.OK.value)  {
-				Log.i("ConnectAPI", "Success to Get response")
-			}
-			return httpResponse.body() as List<Associated>
-		} catch (e: Exception) {
-			Log.e("ConnectAPI", "Failed to get response", e)
-			e.printStackTrace()
-			return emptyList()
-		}
-	}
-
-	suspend fun postAssociated(associated: Associated): Boolean {
-		try {
-			val httpResponse: HttpResponse = client.post("$BASE_URL/associated") {
-				contentType(ContentType.Application.Json)
-				setBody(associated)
-			}
-			if (httpResponse.status.value == HttpStatusCode.Conflict.value) {
-				Log.e("ConnectAPI", "Failed to get response: ${httpResponse.body() as String}")
-				return false
-			}
-			if (httpResponse.status.value == HttpStatusCode.BadRequest.value) {
-				Log.e("ConnectAPI", "Failed to get response: ${httpResponse.body() as String}")
-				return false
-			}
-			if (httpResponse.status.value == HttpStatusCode.OK.value) {
-				Log.i("ConnectAPI", "Success to Get response")
-				return true
-			}
-			return true
-		} catch (e: Exception) {
-			Log.e("ConnectAPI", "Failed to get response", e)
-			e.printStackTrace()
-			return false
-		}
-	}
-}
-
 
 @Composable
-fun FactoryAssociated(associated: Associated?) {
+fun FactoryAssociated(associated: AssociatedEntity?) {
 	Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(16.dp)) {
 		Text("FOTO AQUI!!!!", modifier = Modifier.padding(end = 16.dp))
 		Column {
@@ -118,44 +38,16 @@ fun FactoryAssociated(associated: Associated?) {
 }
 
 @Composable
-fun ConnectAPI(modifier: Modifier = Modifier) {
-	var associatesList by remember { mutableStateOf<List<Associated>>(emptyList()) }
-	var isLoading by remember { mutableStateOf(true) }
-	var errorMessage by remember { mutableStateOf<String?>(null) }
-	val scope = rememberCoroutineScope()
+fun ConnectAPI(
+	modifier: Modifier = Modifier,
+) {
+	//val associatesListState by viewModel.associatesList.collectAsState()
+	var isRefreshingManually by remember { mutableStateOf(false) }
 
-	suspend fun loadAssociated() {
-		isLoading = true
-		errorMessage = null
-		try {
-			val result = withContext(Dispatchers.IO) {
-				ApiService.getAllAssociated()
-			}
-			associatesList = result
-		} catch (e: Exception) {
-			errorMessage = "Falha ao carregar dados: ${e.message}"
-			Log.e("ConnectAPI", "Erro na chamada da API", e)
-		} finally {
-			isLoading = false
-		}
-	}
-
-	LaunchedEffect(Unit) {
-		loadAssociated()
-	}
 
 	Scaffold(modifier = modifier, bottomBar = {
 		ButtonFormAdd(modifier, onClick = {
-			val newAssociated = Associated(
-				name = "test",
-				groupId = 11,
-				numberCard = 15
-			)
-			scope.launch {
-				if (ApiService.postAssociated(newAssociated)) {
-					loadAssociated()
-				}
-			}
+			//TODO Adicionar ação ao clicar no botão
 		})
 	}
 	) { paddingValues ->
@@ -172,14 +64,20 @@ fun ConnectAPI(modifier: Modifier = Modifier) {
 			) {
 				IconButton(
 					onClick = {
-						scope.launch { loadAssociated() }
+						isRefreshingManually = true
+						//viewModel.refreshDataFromServer()
 					},
-					enabled = !isLoading
+					enabled = !isRefreshingManually
 				) {
-					Icon(
-						imageVector = Icons.Filled.Refresh,
-						contentDescription = "Recarregar"
-					)
+					if (isRefreshingManually) {
+						CircularProgressIndicator(modifier = Modifier.size(24.dp))
+						//TODO adicionar uma verificação de tempo e exibir uma mensagem de aviso
+					} else {
+						Icon(
+							imageVector = Icons.Filled.Refresh,
+							contentDescription = "Recarregar"
+						)
+					}
 				}
 			}
 
@@ -189,36 +87,8 @@ fun ConnectAPI(modifier: Modifier = Modifier) {
           .weight(1f),
 				contentAlignment = Alignment.Center
 			) {
-				when {
-					isLoading -> {
-						CircularProgressIndicator()
-					}
-
-					errorMessage != null -> {
-						Column(horizontalAlignment = Alignment.CenterHorizontally) {
-							Text("Erro: $errorMessage")
-							Button(onClick = {
-								scope.launch { loadAssociated() }
-							}) {
-								Text("Tentar Novamente")
-							}
-						}
-					}
-
-					associatesList.isEmpty() && !isLoading -> {
-						Text("Nenhuma pessoa encontrada.")
-					}
-
-					else -> {
-						LazyColumn(modifier = Modifier.fillMaxSize()) {
-							items(associatesList) { associated ->
-								FactoryAssociated(associated)
-							}
-						}
-					}
-				}
+				//TODO adicionar a lista de associados
 			}
-
 		}
 	}
 }
