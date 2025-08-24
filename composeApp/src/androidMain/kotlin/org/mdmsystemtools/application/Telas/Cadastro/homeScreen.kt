981548c888049d18bbb35e22494356bf8758863e
Matheus <Matheusssousaribeiro@gmail.com>
2025-08-24 14:42:09 -0300

    Update home screen layout

--- composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Cadastro/homeScreen.kt
+++ composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Cadastro/homeScreen.kt
@@ -1,23 +1,33 @@
 package org.mdmsystemtools.application.Telas.Cadastro
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import org.mdmsystemtools.application.componentes.ButtonFormAdd
+import org.mdmsystemtools.application.componentes.SearchBar
 
 @Composable
 fun CadastrosScreen() {
   var showDialog by remember { mutableStateOf(false) }
 	val modifier: Modifier = Modifier
 
-  ButtonFormAdd(modifier, onClick = { showDialog = true })
+  Box(modifier = Modifier.fillMaxSize()) {
+    SearchBar(
+      placeholder = "Pesquisar cadastros...",
+      onSearchChange = { searchText ->
+      }
+    )
 
-  Formulario(
-    showDialog = showDialog,
-    onDismiss = { showDialog = false }
-  )
+    Formulario(
+      showDialog = showDialog,
+      onDismiss = { showDialog = false }
+    )
+
+    ButtonFormAdd(Modifier.fillMaxSize(), onClick = { showDialog = true })
+  }
 }
