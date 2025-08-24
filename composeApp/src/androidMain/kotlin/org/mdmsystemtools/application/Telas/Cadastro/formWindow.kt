f8c0bc9b5aea64bcc81b920f7ed7a881707c776c
Matheus <Matheusssousaribeiro@gmail.com>
2025-08-24 14:39:31 -0300

    Update registration form screen

--- composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Cadastro/formWindow.kt
+++ composeApp/src/androidMain/kotlin/org/mdmsystemtools/application/Telas/Cadastro/formWindow.kt
@@ -1,15 +1,24 @@
 package org.mdmsystemtools.application.Telas.Cadastro
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
@@ -18,10 +27,15 @@ import androidx.compose.ui.window.Dialog
 fun Formulario (
     showDialog: Boolean,
     onDismiss: () -> Unit,
-    title: String = "Esta é a tela de detalhes que abriu por cima!",
-    buttonText: String = "Fechar"
+    title: String = "Cadastro",
+    buttonText: String = "Salvar"
 ) {
     if (showDialog) {
+        var nome by remember { mutableStateOf("") }
+        var email by remember { mutableStateOf("") }
+        var telefone by remember { mutableStateOf("") }
+        var cpf by remember { mutableStateOf("") }
+
         Dialog(
             onDismissRequest = onDismiss
         ) {
@@ -29,6 +43,10 @@ fun Formulario (
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(20.dp)
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(16.dp)
+                    )
             ) {
                 Column(
                     modifier = Modifier.padding(20.dp),
@@ -37,10 +55,42 @@ fun Formulario (
                 ) {
                     Text(
                         text = title,
-                        fontSize = 16.sp
+                        fontSize = 18.sp
                     )
+                    
+                    OutlinedTextField(
+                        value = nome,
+                        onValueChange = { nome = it },
+                        label = { Text("Nome") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    OutlinedTextField(
+                        value = cpf,
+                        onValueChange = { nome = it },
+                        label = { Text("CPF") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    
+                    OutlinedTextField(
+                        value = email,
+                        onValueChange = { email = it },
+                        label = { Text("Email") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    
+                    OutlinedTextField(
+                        value = telefone,
+                        onValueChange = { telefone = it },
+                        label = { Text("Telefone") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    
                     Button(
-                        onClick = onDismiss
+                        onClick = {
+                            onDismiss()
+                        },
+                        modifier = Modifier.fillMaxWidth()
                     ) {
                         Text(buttonText)
                     }
