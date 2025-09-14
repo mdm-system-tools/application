353a7e76ffe65a08e02890bb79c3a06261b920b8
Eduardo Silva <eduardofox989@protonmail.com>
2025-09-14 19:07:27 -0300

    moved all content from screenMain to mainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,23 +3,52 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.mdmsystemtools.application.presentation.ui.screens.telaPrincipal.TelaPrincipal
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContent {
-      MaterialTheme {
-        Surface {
-          val navController= rememberNavController()
-          TelaPrincipal(navController = navController)
-        }
-      }
-    }
-  }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContent {
+			MaterialTheme {
+				val navController = rememberNavController()
+				val startDestination = Destination.CADASTROS
+				var selectedDestination by rememberSaveable { mutableIntStateOf(startDestination.ordinal) }
+
+				Scaffold(
+					bottomBar = {
+						NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+							Destination.entries.forEachIndexed { index, destination ->
+								NavigationBarItem(selected = selectedDestination == index, onClick = {
+									navController.navigate(route = destination.route)
+									selectedDestination = index
+								}, icon = {
+									Icon(
+										destination.icon, contentDescription = destination.contentDescription
+									)
+								}, label = { Text(destination.label) })
+							}
+						}
+					}) { contentPadding ->
+					AppNavHost(navController, startDestination, modifier = Modifier.padding(contentPadding))
+				}
+			}
+		}
+	}
 }
