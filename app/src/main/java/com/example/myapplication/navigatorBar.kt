573a7ec0031e1e54b0c726a2d6635f28fe200136
Eduardo Silva <eduardofox989@protonmail.com>
2025-09-14 19:01:20 -0300

    change: CAMERA to CONFIGURACAO on navigatorbar

--- app/src/main/java/com/example/myapplication/navigatorBar.kt
+++ app/src/main/java/com/example/myapplication/navigatorBar.kt
@@ -2,49 +2,52 @@ package com.example.myapplication
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CalendarToday
-import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.People
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import org.mdmsystemtools.application.presentation.ui.screens.Cadastro.CadastrosScreen
-import org.mdmsystemtools.application.presentation.ui.screens.Cadastro.Formulario
-import org.mdmsystemtools.application.presentation.ui.screens.Camera.CameraScreen
+import org.mdmsystemtools.application.presentation.ui.screens.Cadastro.ProfilesListScreen
+import org.mdmsystemtools.application.presentation.ui.screens.Cadastro.FormScreen
+import org.mdmsystemtools.application.presentation.ui.screens.Camera.SettingsScreen
 import org.mdmsystemtools.application.presentation.ui.screens.Reunião.ReuniaoNavigation
 
 enum class Destination(
-    val route: String,
-    val label: String,
-    val icon: ImageVector,
-    val contentDescription: String
+	val route: String,
+	val label: String,
+	val icon: ImageVector,
+	val contentDescription: String
 ) {
-    CADASTROS("Cadastros", "Cadastros", Icons.Default.People, "Cadastros"),
-    CAMERA("Camera", "Camera", Icons.Default.CameraAlt, "Camera"),
-    REUNIAO("Reuniao", "Reuniao", Icons.Default.CalendarToday, "Reuniao"),
+	CADASTROS("Cadastros", "Cadastros", Icons.Default.People, "Cadastros"),
+	REUNIAO("Reuniao", "Reuniao", Icons.Default.CalendarToday, "Reuniao"),
+	CONFIGURACAO("Configuração", "Configuração", Icons.Default.Settings, "Configuração"),
 }
 
 @Composable
 fun AppNavHost(
-    navController: NavHostController,
-    startDestination: Destination,
-    modifier: Modifier = Modifier
+	navController: NavHostController,
+	startDestination: Destination,
+	modifier: Modifier = Modifier
 ) {
-    NavHost(
-        navController,
-        startDestination = startDestination.route
-    ) {
-        Destination.entries.forEach { destination ->
-            composable(destination.route) {
-                when (destination) {
-                    Destination.CADASTROS -> CadastrosScreen(navController)
-                    Destination.CAMERA -> CameraScreen()
-                    Destination.REUNIAO -> ReuniaoNavigation()
-                }
-            }
-        }
-        composable("Formulario") { Formulario(modifier) }
-    }
+	NavHost(
+		navController,
+		startDestination = startDestination.route
+	) {
+		Destination.entries.forEach { destination ->
+			composable(destination.route) {
+				when (destination) {
+					Destination.CADASTROS -> ProfilesListScreen(navController)
+					Destination.REUNIAO -> ReuniaoNavigation()
+					Destination.CONFIGURACAO -> SettingsScreen()
+				}
+			}
+		}
+		// TODO REFATORING Melhorar a forma de lidar com mutiplas telas
+		composable("Formulario") {
+			FormScreen()
+		}
+	}
 }
