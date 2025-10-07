b3ace02042f13f4e64a029d69cd3d664e3c72cd9
Eduardo Silva <eduardofox989@protonmail.com>
2025-10-07 18:03:06 -0300

    refactoring of the associatelistscreen screen

--- app/src/main/java/org/MdmSystemTools/Application/view/screens/Registration/AssociateListScreen.kt
+++ app/src/main/java/org/MdmSystemTools/Application/view/screens/Registration/AssociateListScreen.kt
@@ -4,48 +4,38 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import org.MdmSystemTools.Application.view.components.Common.ButtonFormAdd
 import org.MdmSystemTools.Application.view.components.Registration.AssociateProfile
 import org.MdmSystemTools.Application.view.components.Registration.QuickAccessButtons
-import org.MdmSystemTools.Application.view.components.Registration.SearchBar
 
 
 @Composable
 fun AssociateListScreen(
 	viewModel: AssociateListViewModel = hiltViewModel(),
-	onClickFloatingBottom: () -> Unit
+	modifier: Modifier
 ) {
 	val listAssociates by viewModel.listAssociates.collectAsState()
 
-	Surface {
-		ButtonFormAdd(onClick = onClickFloatingBottom)
+	Column(modifier) {
 
-		Column {
-			SearchBar(
-				placeholder = "Pesquisar cadastros...", onSearchChange = { searchText ->
-				})
+		QuickAccessButtons(
+			onLastMeetingClick = { },
+			onLastSearchClick = { },
+		)
 
-			QuickAccessButtons(
-				onLastMeetingClick = { /* TODO */ },
-				onLastSearchClick = { /* TODO */ },
-			)
-
-			LazyColumn(
-				modifier = Modifier
-					.padding(
-						start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp
-					)
-			) {
-				items(listAssociates) { associate ->
-					AssociateProfile(associate)
-				}
+		LazyColumn(
+			modifier = Modifier
+				.padding(
+					start = 16.dp, top = 16.dp, end = 16.dp, bottom = 16.dp
+				)
+		) {
+			items(listAssociates) { associate ->
+				AssociateProfile(associate)
 			}
 		}
 	}
