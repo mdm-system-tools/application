9e071a29d1b6c51e1ab1b9ed7368796c26b69be2
Eduardo Silva <eduardofox989@protonmail.com>
2025-10-09 16:46:14 -0300

    added suite materialui3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,7 @@ dependencies {
   implementation(libs.androidx.navigation.compose)
   implementation(libs.androidx.foundation.layout)
   implementation(libs.androidx.animation)
+  implementation(libs.androidx.material3.adaptive.navigation.suite)
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junit)
   androidTestImplementation(libs.androidx.espresso.core)
