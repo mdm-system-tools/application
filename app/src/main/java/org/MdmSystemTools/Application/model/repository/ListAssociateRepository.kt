cbf8a303639d9cbb845573d8dae0c99c3eb0844c
Eduardo Silva <eduardofox989@protonmail.com>
2025-10-17 16:57:21 -0300

    rename function on interface associateRepository

--- app/src/main/java/org/MdmSystemTools/Application/model/repository/ListAssociateRepository.kt
+++ app/src/main/java/org/MdmSystemTools/Application/model/repository/ListAssociateRepository.kt
@@ -5,5 +5,5 @@ import org.MdmSystemTools.Application.model.dto.AssociateDto
 interface ListAssociateRepository {
   fun getListAssociates(): List<AssociateDto>
   fun getAssociate(id: Int): AssociateDto?
-	fun createAssociate(associate: AssociateDto)
+	fun addAssociate(associate: AssociateDto)
 }
