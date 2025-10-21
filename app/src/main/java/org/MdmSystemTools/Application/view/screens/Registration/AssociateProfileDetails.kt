1b5e8aa126db19e86c8363d64f14b3b4ff42bc20
Eduardo Silva <eduardofox989@protonmail.com>
2025-10-21 17:17:45 -0300

    adds scaffold and appbar with back button to associateprofile

--- app/src/main/java/org/MdmSystemTools/Application/view/screens/Registration/AssociateProfileDetails.kt
+++ app/src/main/java/org/MdmSystemTools/Application/view/screens/Registration/AssociateProfileDetails.kt
@@ -2,12 +2,13 @@ package org.MdmSystemTools.Application.view.screens.Registration
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -17,26 +18,27 @@ import androidx.compose.ui.Modifier
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AssociateProfileDetails(onClickBackScreen: () -> Unit) {
-	TopAppBar(
-		title = {
-			Text("Associate Profile")
-		},
-		actions = {
-			IconButton(
-				onClick = onClickBackScreen,
-			) {
-				Icon(
-					Icons.AutoMirrored.Filled.ArrowBack,
-					contentDescription = null,
-					tint = MaterialTheme.colorScheme.onSurfaceVariant,
-				)
-			}
-		},
-	)
-	Box(
-		modifier = Modifier.fillMaxSize(),
-		contentAlignment = Alignment.Center
-	) {
-		Text("TODO Implementar uma tela")
+	Scaffold(
+		topBar = {
+			TopAppBar(
+				title = {
+					Text("Associate Profile")
+				},
+				navigationIcon = {
+					IconButton(onClick = onClickBackScreen) {
+						Icon(Icons.AutoMirrored.Filled.ArrowBack, null)
+					}
+				}
+			)
+		}
+	) { paddingValues ->
+		Box(
+			modifier = Modifier
+        .fillMaxSize()
+        .padding(paddingValues),
+			contentAlignment = Alignment.Center
+		) {
+			Text("TODO Implementar uma tela")
+		}
 	}
 }
