db6b104ab6cea201232ca147f528adf7c67ab7d4
Eduardo Silva <eduardofox989@protonmail.com>
2025-10-22 12:26:56 -0300

    adds spacing and arrow icon to associate profile

--- app/src/main/java/org/MdmSystemTools/Application/view/components/Registration/AssociateProfile.kt
+++ app/src/main/java/org/MdmSystemTools/Application/view/components/Registration/AssociateProfile.kt
@@ -1,14 +1,17 @@
 package org.MdmSystemTools.Application.view.components.Registration
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,37 +28,56 @@ import org.MdmSystemTools.Application.view.theme.AppConstants
 @Composable
 fun AssociateProfile(associated: AssociateDto, onClick: () -> Unit) {
 	Card(
-		modifier = Modifier
-      .height(64.dp)
-      .width(270.dp), onClick = onClick
+		modifier = Modifier.fillMaxWidth(),
+		onClick = onClick
 	) {
 		Row(
+			modifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp),
 			verticalAlignment = Alignment.CenterVertically,
+			horizontalArrangement = Arrangement.SpaceBetween
 		) {
-			Image(
-				painter = painterResource(id = R.drawable.ic_launcher_background),
-				contentDescription = null,
-				modifier = Modifier
-          .size(64.dp)
-          .clip(CircleShape)
-			)
-			Column(Modifier.padding(start = 8.dp)) {
-				Text(
-					associated.name,
-					fontSize = AppConstants.FontSize.medium,
-					fontWeight = FontWeight(500),
-					maxLines = 1
-				)
-				Row {
-					Text(
-						"Grupo: ${associated.groupId} Carterinha: ${associated.numberCard}",
-						fontSize = AppConstants.FontSize.small,
-						fontWeight = FontWeight(400)
-					)
-				}
+			Row(
+				modifier = Modifier,
+				verticalAlignment = Alignment.CenterVertically,
+			) {
+				ImageProfile()
+				InformationToAssociate(associated)
 			}
+			Icon(Icons.AutoMirrored.Filled.ArrowForward, null)
 		}
+	}
+}
 
+@Composable
+private fun ImageProfile() {
+	Image(
+		painter = painterResource(id = R.drawable.ic_launcher_background),
+		contentDescription = null,
+		modifier = Modifier
+      .padding(all = 6.dp)
+      .size(32.dp)
+      .clip(CircleShape)
+	)
+}
+
+@Composable
+private fun InformationToAssociate(associated: AssociateDto) {
+	Column(Modifier.padding(start = 8.dp)) {
+		Text(
+			associated.name,
+			fontSize = AppConstants.FontSize.medium,
+			fontWeight = FontWeight(500),
+			maxLines = 1
+		)
+		Row {
+			Text(
+				"Grupo: ${associated.groupId} Carterinha: ${associated.numberCard}",
+				fontSize = AppConstants.FontSize.small,
+				fontWeight = FontWeight(400)
+			)
+		}
 	}
 }
 
