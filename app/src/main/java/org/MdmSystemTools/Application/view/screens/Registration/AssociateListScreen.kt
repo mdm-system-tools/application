547a443bd198c3ecd09934d733f04d43ffd4d22c
Eduardo Silva <eduardofox989@protonmail.com>
2025-10-22 21:38:52 -0300

    updated ListAssociateScreen adds filter button and changes floating button to a button at the top

--- app/src/main/java/org/MdmSystemTools/Application/view/screens/Registration/AssociateListScreen.kt
+++ app/src/main/java/org/MdmSystemTools/Application/view/screens/Registration/AssociateListScreen.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -16,68 +17,44 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import org.MdmSystemTools.Application.model.dto.AssociateDto
 import org.MdmSystemTools.Application.view.components.Registration.AssociateProfile
-import org.MdmSystemTools.Application.view.theme.AppConstants
 
 
 @Composable
 fun AssociateListScreen(
 	viewModel: AssociateListViewModel = hiltViewModel(),
 	onClickAssociateProfile: (associateId: Int) -> Unit,
-	onClickFloatingButtom: () -> Unit
+	onClickFloatingButton: () -> Unit
 ) {
 	val listAssociates by viewModel.listAssociates.collectAsState()
-	ListAssociates(onClickFloatingButtom, listAssociates, onClickAssociateProfile)
+	ListAssociates(onClickFloatingButton, listAssociates, onClickAssociateProfile)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun ListAssociates(
-	onClickFloatingButton: () -> Unit,
+	onClickAdd: () -> Unit,
 	listAssociates: List<AssociateDto>,
 	onClickAssociateProfile: (Int) -> Unit
 ) {
 	Scaffold(
-		topBar = {
-			TopAppBar(
-				colors = TopAppBarDefaults.topAppBarColors(
-					containerColor = Color(0xFF1C6AEA),
-					titleContentColor = Color.White
-				),
-				title = {
-					Column {
-						Text(
-							"Associate",
-							fontSize = AppConstants.FontSize.large,
-							fontWeight = FontWeight(500)
-						)
-						Text(
-							"Gerencie Associados, Groupos e Projetos",
-							fontSize = AppConstants.FontSize.medium,
-							fontWeight = FontWeight(400)
-						)
-					}
-				}
-			)
-		}
+
 	) { innerPadding ->
 		Column(
 			modifier = Modifier.padding(innerPadding)
 		) {
 			//SearchBar() // TODO Adicionar isso
-			ButtonsActions()
+			ButtonsActions(onClickAdd)
 			LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp)) {
 				itemsIndexed(listAssociates) { index, associate ->
 					Log.i("associateListScreen", "index de associado $index")
@@ -92,9 +69,16 @@ private fun ListAssociates(
 }
 
 @Composable
-private fun ButtonsActions() {
-	Row {
+private fun ButtonsActions(onClick: () -> Unit) {
+	Row(
+		modifier = Modifier
+      .fillMaxWidth()
+      .padding(8.dp),
+		verticalAlignment = Alignment.CenterVertically,
+		horizontalArrangement = Arrangement.SpaceBetween
+	) {
 		Card(
+			//TODO Implementar uma filtro
 			{}, colors = CardDefaults.cardColors(
 				containerColor = Color.Transparent,
 				contentColor = Color(0xFFB7B6B6)
@@ -104,7 +88,7 @@ private fun ButtonsActions() {
 			Text("Filtros")
 		}
 		Card(
-			{}, colors = CardDefaults.cardColors(
+			onClick, colors = CardDefaults.cardColors(
 				containerColor = Color(0xFF1C6AEA),
 				contentColor = Color.White
 			)
@@ -119,5 +103,5 @@ private fun ButtonsActions() {
 @Composable
 private fun AssociateListScreenPreview() {
 	val list = listOf(AssociateDto("jose", 1, 1), AssociateDto("maria", 1, 1))
-	ListAssociates(onClickAssociateProfile = {}, onClickFloatingButton = {}, listAssociates = list)
+	ListAssociates(onClickAssociateProfile = {}, onClickAdd = {}, listAssociates = list)
 }
