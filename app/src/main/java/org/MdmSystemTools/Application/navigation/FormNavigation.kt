da7882dd7d742a783423897f814de70abb907db0
Matheus <matheusssousaribeiro@gmail.com>
2025-10-26 12:29:37 -0300

    Only UI import and a title

--- app/src/main/java/org/MdmSystemTools/Application/navigation/FormNavigation.kt
+++ app/src/main/java/org/MdmSystemTools/Application/navigation/FormNavigation.kt
@@ -1,20 +1,25 @@
 package org.MdmSystemTools.Application.navigation
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
-import org.MdmSystemTools.Application.view.screens.Calendar.FormEventScreen
 
 @Serializable
 object AddEvent
 
 fun NavGraphBuilder.addEvent() {
 	composable<AddEvent> {
-		//TODO adicionar onEventSaved
-		FormEventScreen(
-			onNavigateBack = {
-			},
-		)
+		Box(
+			modifier = Modifier.fillMaxSize(),
+			contentAlignment = Alignment.Center
+		) {
+			Text("Adicionar Evento")
+		}
 	}
 }
 
