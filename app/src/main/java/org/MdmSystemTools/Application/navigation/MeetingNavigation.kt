2e6c32f859563f51e22daf12f6f5ec55ad7f5e83
Eduardo Silva <eduardofox989@protonmail.com>
2025-10-31 21:31:44 -0300

    implementa navegação para MeetingDetailsScreen

--- app/src/main/java/org/MdmSystemTools/Application/navigation/MeetingNavigation.kt
+++ app/src/main/java/org/MdmSystemTools/Application/navigation/MeetingNavigation.kt
@@ -4,24 +4,25 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
-import org.MdmSystemTools.Application.view.screens.Meeting.MeetingDetails
-import org.MdmSystemTools.Application.view.screens.Meeting.RollCall
+import org.MdmSystemTools.Application.view.screens.Meeting.MeetingDetailsScreen
+import org.MdmSystemTools.Application.view.screens.Meeting.RollCallScreen
 
 @Serializable internal object MeetingDetails
+
 @Serializable internal object RollCall
 
 fun NavGraphBuilder.meetingDetails(onClickBack: () -> Unit, onClick: () -> Unit) {
-	composable<MeetingDetails> { MeetingDetails(onClickBack, onClick) }
+  composable<MeetingDetails> { MeetingDetailsScreen(onClickBack, onClick) }
 }
 
 fun NavController.navigateToMeetingDetails() {
-	navigate(MeetingDetails)
+  navigate(MeetingDetails)
 }
 
 fun NavGraphBuilder.meetingRollCall(onClickBack: () -> Unit) {
-	composable<RollCall> { RollCall(onClickBack = onClickBack) }
+  composable<RollCall> { RollCallScreen(onClickBack = onClickBack) }
 }
 
 fun NavController.navigateToMeetingRollCall() {
-	navigate(RollCall)
+  navigate(RollCall)
 }
