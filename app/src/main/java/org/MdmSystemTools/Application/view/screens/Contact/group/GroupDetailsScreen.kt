772097779196fb8466c1a2360b2bcc8b96a7685c
Eduardo Silva <eduardofox989@protonmail.com>
2025-11-02 19:03:08 -0300

    adicionar tela de detalhes e formulario para grupo

---
+++ app/src/main/java/org/MdmSystemTools/Application/view/screens/Contact/group/GroupDetailsScreen.kt
@@ -0,0 +1,13 @@
+package org.MdmSystemTools.Application.view.screens.Contact.group
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GroupDetailsScreen(
+  onClickBackScreen: () -> Unit,
+  onClickConfirmButton: () -> Unit,
+  modifier: Modifier = Modifier,
+) {
+
+}
---
+++ app/src/main/java/org/MdmSystemTools/Application/view/screens/Contact/group/GroupFormScreen.kt
@@ -0,0 +1,11 @@
+package org.MdmSystemTools.Application.view.screens.Contact.group
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun GroupFormScreen(
+	onClickBackScreen: () -> Unit,
+	onClickConfirmButton: () -> Unit,
+	modifier: Modifier = Modifier,
+) {}
