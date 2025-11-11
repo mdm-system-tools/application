ac56ab9e279d68bd6782d8974eba009c222b563b
Eduardo Silva <eduardofox989@protonmail.com>
2025-11-10 21:07:12 -0300

    implementa função addProject

--- app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectRepository.kt
+++ app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectRepository.kt
@@ -1,5 +1,8 @@
 package org.MdmSystemTools.Application.model.repository
 
-interface ProjectRepository {
+import org.MdmSystemTools.Application.model.entity.Grupo
+import org.MdmSystemTools.Application.model.entity.Project
 
+interface ProjectRepository {
+	fun addProject(project: Project): Long
 }
--- app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectRepositoryImpl.kt
+++ app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectRepositoryImpl.kt
@@ -1,7 +1,8 @@
 package org.MdmSystemTools.Application.model.repository
 
+import org.MdmSystemTools.Application.model.entity.Project
 import org.MdmSystemTools.Application.model.entity.ProjectDao
 
 class ProjectRepositoryImpl(private val dao : ProjectDao): ProjectRepository {
-
+	override fun addProject(project: Project): Long = dao.insert(project)
 }
