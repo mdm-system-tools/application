52ec40695fd6d9d0ab680654daf327db78fd6b11
Eduardo Silva <eduardofox989@protonmail.com>
2025-11-10 21:10:33 -0300

    pequena formatação nos arquivos

--- app/src/main/java/org/MdmSystemTools/Application/MainActivity.kt
+++ app/src/main/java/org/MdmSystemTools/Application/MainActivity.kt
@@ -6,15 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-	override fun onCreate(savedInstanceState: Bundle?) {
-		super.onCreate(savedInstanceState)
-		setContent {
-			MaterialTheme {
-				App()
-			}
-		}
-	}
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent { MaterialTheme { App() } }
+  }
 }
--- app/src/main/java/org/MdmSystemTools/Application/model/repository/AssociateRepository.kt
+++ app/src/main/java/org/MdmSystemTools/Application/model/repository/AssociateRepository.kt
@@ -3,8 +3,11 @@ package org.MdmSystemTools.Application.model.repository
 import org.MdmSystemTools.Application.model.dto.AssociateDto
 
 interface AssociateRepository {
-	fun getAssociates(): List<AssociateDto>
-	fun getAssociate(id: Int): AssociateDto?
-	fun addAssociate(associate: AssociateDto)
-	fun deleteAssociate(id: Int)
+  fun getAssociates(): List<AssociateDto>
+
+  fun getAssociate(id: Int): AssociateDto?
+
+  fun addAssociate(associate: AssociateDto)
+
+  fun deleteAssociate(id: Int)
 }
