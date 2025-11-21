8555206c175806312cc4d73c56f5236726972f46
Eduardo Silva <eduardofox989@protonmail.com>
2025-11-21 20:02:01 -0300

    implementa projectWith

--- app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectWithGroupsRepository.kt
+++ app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectWithGroupsRepository.kt
@@ -1,4 +1,7 @@
 package org.MdmSystemTools.Application.model.repository
 
+import org.MdmSystemTools.Application.model.entity.ProjectWithGroups
+
 interface ProjectWithGroupsRepository {
+	suspend fun getProjectWithGroups(projectId: Long): ProjectWithGroups
 }
--- app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectWithGroupsRepositoryImpl.kt
+++ app/src/main/java/org/MdmSystemTools/Application/model/repository/ProjectWithGroupsRepositoryImpl.kt
@@ -1,4 +1,11 @@
 package org.MdmSystemTools.Application.model.repository
 
-class ProjectWithGroupsRepositoryImpl {
+import org.MdmSystemTools.Application.model.entity.ProjectWithGroups
+import org.MdmSystemTools.Application.model.entity.ProjectWithGroupsDao
+
+class ProjectWithGroupsRepositoryImpl(private val dao: ProjectWithGroupsDao) :
+	ProjectWithGroupsRepository {
+	override suspend fun getProjectWithGroups(projectId: Long): ProjectWithGroups {
+		return dao.getProjectWithGroups(projectId)
+	}
 }
